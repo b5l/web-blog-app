1beb06574f3b61b369718aa87b33e7be894f6067
Eller Daniel <edanimark@codingsans.com>
2023-01-08 10:32:41 +0100

    login page with auth

--- src/page/Login/Login.tsx
+++ src/page/Login/Login.tsx
@@ -2,18 +2,36 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import {
   Box,
   FormControl,
-  Input,
   Stack,
   Button,
   Text,
   Spacer,
+  Input,
 } from "native-base";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { RootStackParamList } from "../../components/navigation/navigationParams";
 import { loginStyle } from "../../style/login";
+import { getLogin } from "./store/selectors";
+import { fetchLoginAction } from "./store/slice";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Login">;
 
 export const LoginPage = ({ navigation }: Props) => {
+  const dispatch = useDispatch();
+
+  const [formData, setData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  const auth = useSelector(getLogin);
+
+  useEffect(() => {
+    if (auth.isAuth) {
+      navigation.navigate("Posts");
+      setErrorMessage(false);
+    }
+  }, [auth]);
+
   return (
     <>
       <Box style={loginStyle.container}>
@@ -21,24 +39,42 @@ export const LoginPage = ({ navigation }: Props) => {
           Login
         </Text>
         <Box style={loginStyle.inputContainer} m="15">
-          <FormControl isRequired>
+          <FormControl isRequired isInvalid={errorMessage}>
             <Stack mx="10" mb="2">
               <FormControl.Label>Username</FormControl.Label>
-              <Input type="text" placeholder="username" />
+              <Input
+                type="text"
+                placeholder="username"
+                onChangeText={(value) =>
+                  setData({ ...formData, username: value })
+                }
+              />
             </Stack>
           </FormControl>
-          <FormControl isRequired>
+          <FormControl isRequired isInvalid={errorMessage}>
             <Stack mx="10">
               <FormControl.Label>Password</FormControl.Label>
-              <Input type="password" placeholder="password" />
+              <Input
+                type="password"
+                placeholder="password"
+                onChangeText={(value) =>
+                  setData({ ...formData, password: value })
+                }
+              />
             </Stack>
+            <FormControl.ErrorMessage>
+              Login Failed! Try again!
+            </FormControl.ErrorMessage>
           </FormControl>
           <Spacer />
           <Button
             colorScheme="primary"
             shadow={10}
             style={loginStyle.loginButton}
-            onPress={() => navigation.navigate("Posts")}
+            onPress={() => {
+              dispatch(fetchLoginAction(formData));
+              setErrorMessage(true);
+            }}
           >
             Login
           </Button>
