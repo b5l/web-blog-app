d73494b1f1623e2b03f318e616bd3c59bfce8b70
Eller Daniel <edanimark@codingsans.com>
2023-01-11 23:49:16 +0100

    blog delete WIP

--- src/page/BlogDetails/BlogDetails.tsx
+++ src/page/BlogDetails/BlogDetails.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Box, Button, Heading, Text } from "native-base";
-import { useEffect, useState } from "react";
+import { AlertDialog, Box, Button, Heading, Text } from "native-base";
+import { useEffect, useRef, useState } from "react";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { useDispatch, useSelector } from "react-redux";
 import { RootStackParamList } from "../../components/navigation/navigationParams";
@@ -16,6 +16,10 @@ export const BlogDetails = ({ route, navigation }: Props) => {
 
   const dispatch = useDispatch();
   const detailsData = useSelector(getBlogDetails);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = useRef(null);
 
   useEffect(() => {
     dispatch(fetchBlogDetailsAction({ id: id }));
@@ -43,7 +47,7 @@ export const BlogDetails = ({ route, navigation }: Props) => {
         </Text>
       </Box>
       <Button
-        style={style.button}
+        style={style.reverseButton}
         size={"lg"}
         onPress={() => {
           navigation.navigate("Edit", { edit: true, type: data?.type });
@@ -51,6 +55,39 @@ export const BlogDetails = ({ route, navigation }: Props) => {
       >
         <MaterialCommunityIcons name="pencil" color="white" size={25} />
       </Button>
+      <Button
+        colorScheme="danger"
+        style={style.button}
+        onPress={() => setIsOpen(!isOpen)}
+      >
+        <MaterialCommunityIcons name="delete" color="white" size={27} />
+      </Button>
+      <AlertDialog
+        leastDestructiveRef={cancelRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <AlertDialog.Content>
+          <AlertDialog.CloseButton />
+          <AlertDialog.Header>Delete Blog post</AlertDialog.Header>
+          <AlertDialog.Body>Are you sure?</AlertDialog.Body>
+          <AlertDialog.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="unstyled"
+                colorScheme="coolGray"
+                onPress={onClose}
+                ref={cancelRef}
+              >
+                No
+              </Button>
+              <Button colorScheme="danger" onPress={onClose}>
+                Yes
+              </Button>
+            </Button.Group>
+          </AlertDialog.Footer>
+        </AlertDialog.Content>
+      </AlertDialog>
     </>
   );
 };
