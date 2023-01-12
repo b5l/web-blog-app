5d4af9a0b43652a99146f58c50c6fb08e41f4118
Eller Daniel <edanimark@codingsans.com>
2023-01-12 20:24:43 +0100

    application run repair

--- src/page/BlogDetails/BlogDetails.tsx
+++ src/page/BlogDetails/BlogDetails.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Box, Button, Heading, Text } from "native-base";
 import { useEffect, useState } from "react";
