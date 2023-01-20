95e910a4ebe34241f3672d466cf550c5445b18f1
Eller Daniel <edanimark@codingsans.com>
2023-01-20 18:33:29 +0100

    style import remove

--- src/page/BlogEdit/BlogEdit.tsx
+++ src/page/BlogEdit/BlogEdit.tsx
@@ -3,7 +3,6 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Box } from "native-base";
 import { BlogPostForm } from "../../components/blogPostForm/blogPostForm";
 import { RootStackParamList } from "../../components/navigation/navigationParams";
-import { style } from "../../style/global";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Edit">;
 
